134bbe2c6c922f492b0ff82c9085ae03442d5370
Sugit <sgmt.snj@gmail.com>
2021-09-17 00:53:10 +0900

    supabase auth trial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Supabase.initialize(
+    url: 'https://qduzbkpvosaiyhvpupuk.supabase.co', //'SUPABASE_URL',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTcxMTg4MCwiZXhwIjoxOTQ3Mjg3ODgwfQ.ZPvvK7lqx7xJ3iCof5s2xQJ72YJBegukvAhAs7EKpB8', //'SUPABASE_ANNON_KEY',
+  );
   runApp(const MyApp());
 }
 
@@ -9,56 +16,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Supabase Trial',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Supabase Trial'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              'Supabase Email Auth',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              child: const Text('Email'),
+              onPressed: () => {
+                supabaseSignUp('sugitlab@gmail.com', '12345678'),
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+void supabaseSignin(String email, String password) async {
+  final res = await Supabase.instance.client.auth
+      .signIn(email: email, password: password);
+  if (res.error != null) {
+    print('error');
+    print(res.error?.message);
+  } else {
+    print('success');
+  }
+}
+
+void supabaseSignUp(String email, String password) async {
+  final res = await Supabase.instance.client.auth.signUp(email, password);
+  if (res.error != null) {
+    print('error, signup');
+    print(res.error?.message);
+    // A user with this email address has already been registered": email-in-use
+  } else {
+    print('success, signup');
   }
 }
